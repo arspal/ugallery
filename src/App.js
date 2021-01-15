8dfec407e650dec45d2c2c8fbca447e9b0a2c3ef
Arseny M <mail@arsm.me>
2021-01-15 12:51:27 +0300

    add node-sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
+import images from './test-data.json';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ugallery">
+      <header className="ugallery__header"><h1>Unsplash Gallery</h1></header>
+      <main className="ugallery_content">
+        <ul>
+          {images.map(() => <li>test-test</li>)}
+        </ul>
+        Photos
+      </main>
     </div>
   );
 }
-
-export default App;
